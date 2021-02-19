4dc2e30c9d8913ebfdaa5d221ccf6c490e90f54a
FelipexxS <felipexx480@gmail.com>
2021-02-18 22:50:49 -0300

    README criado

---
+++ README.md
@@ -0,0 +1,7 @@
+# E-commerce Simples usando JSP e Servlets
+
+**OBS: Este é um projeto para a cadeira de Tópicos em Java para a Web**
+
+## Configurar a aplicação
+Após adicionar o projeto no seu Eclipse, basta configurar a conexão com o database do seu banco de dados MySQL. Vá em *Java Resources > java > br.edu.ifce.utils > BeanUtilities.java* e defina o "dbName", "uName" e "pwd" do seu database. 
+Logo após isso, é preciso usar os arquivos sql com o código em SQL para criar as tabelas e popular a tabela produto, vá em *src > main > resources > database*. Lá haverá dois arquivos sql, importe no seu PhpAdmin ou copie e execute. 
--- SimpleEcommerce/src/main/resources/database/ecommerce-tables.sql
+++ SimpleEcommerce/src/main/resources/database/ecommerce-tables.sql
@@ -20,21 +20,6 @@ CREATE TABLE cliente (
     CONSTRAINT PKCLIENTE PRIMARY KEY (idcliente)
 );
 
-CREATE TABLE carrinho (
-	idcarrinho INT NOT NULL AUTO_INCREMENT,
-    cliente_idcliente INT NOT NULL,
-    CONSTRAINT PKCARRINHO PRIMARY KEY (idcarrinho)
-);
 
-CREATE TABLE carrinho_produto (
-	carrinho_idcarrinho INT NOT NULL,
-    produto_idproduto INT NOT NULL,
-    quantidade INT NOT NULL,
-
-    CONSTRAINT PKCAPRO PRIMARY KEY (carrinho_idcarrinho, produto_idproduto)
-);
 
 ALTER TABLE produto ADD CONSTRAINT PRODUTO_INDICADO FOREIGN KEY (produto_indicado) REFERENCES produto(idproduto);
-ALTER TABLE carrinho ADD CONSTRAINT CARRINHO_CLIENTE FOREIGN KEY (cliente_idcliente) REFERENCES cliente(idcliente);
-ALTER TABLE carrinho_produto ADD CONSTRAINT CARRINHOID FOREIGN KEY (carrinho_idcarrinho) REFERENCES carrinho(idcarrinho);
-ALTER TABLE carrinho_produto ADD CONSTRAINT CARRINHO_PRODUTO FOREIGN KEY (produto_idproduto) REFERENCES produto(idproduto);
